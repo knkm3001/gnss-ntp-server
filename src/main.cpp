35622964a7e54d52e537f5c61be36edf132a7c2f
kazuki s <1540rnkk@gmail.com>
2020-12-27 04:15:23 +0900

    リクエスト元に対応して動的に送信IPを変える

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,20 @@
 
 
 static WiFiUDP wifiUdp;
-static const char *remoteIpadr = "192.168.0.255";
-static const int remoteUdpPort = 9000;
 IPAddress ip(192, 168, 0, 128);
 IPAddress subnet(255, 255, 255, 0);
 IPAddress gateway(192,168, 0, 1);
 IPAddress DNS(192, 168, 0, 1);
 
-
+char packetBuffer[255];
+static const int remoteUdpPort = 9000;
 
 SoftwareSerial mySerial(5, 4); // RX, TX
  
 void setup() {
-  // Open serial communications and wait for port to open:
   Serial.begin(57600);
   while (!Serial) {
-  ; // wait for serial port to connect. Needed for native USB port only
+  ;
   }
 
   Serial.print("Connecting to ");
@@ -33,7 +31,7 @@ void setup() {
       Serial.print(".");
   }
 
-  static const int localPort = 7000;
+  static const int localPort = 123; //ntp port no.
   wifiUdp.begin(localPort);
   
   Serial.println("");
@@ -42,23 +40,29 @@ void setup() {
   Serial.println(WiFi.localIP());
   Serial.println("");
 
- 
  // set the data rate for the SoftwareSerial port
  mySerial.begin(9600);
- mySerial.println("Hello, world?");
+
 }
  
-void loop() { // run over and over
-
-  if (wifiUdp.parsePacket() == 0) {
+void loop() {
+  int packetSize = wifiUdp.parsePacket();
+  if (packetSize==0) {
     delay(100);
     return; 
   }
 
+  // リクエストデータの処理
   Serial.println("packet received!");
-  Serial.println(wifiUdp.read());
+  IPAddress remoteUdpIp = wifiUdp.remoteIP();
+  Serial.println(remoteUdpIp);
+  Serial.println(wifiUdp.remotePort());
+  int len = wifiUdp.read(packetBuffer, packetSize);
+  if (len > 0) packetBuffer[len] = '\0';
+  Serial.println(packetBuffer);
 
-  wifiUdp.beginPacket(remoteIpadr, remoteUdpPort);
+  // 送信データの処理
+  wifiUdp.beginPacket(remoteUdpIp, remoteUdpPort);
   while(1){
     if (mySerial.available()) {
       String serialStr = mySerial.readStringUntil('\n');
